b8104d6f9e87a33721fbaf6dfb0c84f041a6bed4
bokukun01 <asanamisogabe@outlook.jp>
2023-11-29 10:42:33 +0900

    完成

--- main.ts
+++ main.ts
@@ -2,7 +2,8 @@ function checkForError(value: number): void {
     if (value === -1) {
         throw new Error("値が-1なのでエラーです");
     }
-}try {
+}try 
+{
     checkForError(-1);
     console.log("エラーは発生しない");
 } catch (error) {
